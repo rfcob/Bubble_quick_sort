ac7031e7ceb2b064711a875afcfc842196f5dcc1
RFcobo <rafaelcobo@outlook.com>
2025-01-25 19:54:54 -0300

    ;;

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Bubble_quick_sort
+
+## Saída - 3000
+
+- Bubble Sort
+
+        Número de linhas lidas: 30000
+
+        Os 4 pratos com maiores prioridades são: 
+
+        prioridade       tempo      nome
+            
+        221             622        steak-and-ale-pie-953
+        
+        416             530        ploughman's-lunch-390
+        
+        706             106        poitin-154
+        
+        747             338        steak-and-ale-pie-239
+        
+        o tempo de excecução do bubble sort foi em segundos: 0.22884900
+
+- Quick Sort 
+
+        Os 4 pratos com maiores prioridades são: 
+
+        prioridade       tempo      nome
+            
+        221              622        steak-and-ale-pie-953
+        
+        416              530        ploughman's-lunch-390
+        
+        706              106        poitin-154
+        
+        747              338        steak-and-ale-pie-239
+        
+        o tempo de excecução do quick sort foi em segundos: 0.00043900
+
--- main.c
+++ main.c
@@ -5,59 +5,55 @@
 #include <time.h>
 
 
-int main() {
-    // Criar uma instância de cardapioM
-    cardapioM meuCardapio;
+   int main() {
+      // Criar uma instância de cardapioM
+      cardapioM meuCardapio;
 
-    cardapioM meuCardapio_2;
+      cardapioM meuCardapio_2;
 
-    // Abrir o arquivo e contar o numero de linhas
-    openCSV("data/restaurante_pratos_red.csv", ',', &meuCardapio);
+      // Abrir o arquivo e contar o numero de linhas
+      openCSV("data/restaurante_pratos copy.csv", ',', &meuCardapio);
 
-    openCSV("data/restaurante_pratos_red.csv", ',', &meuCardapio_2);
+      openCSV("data/restaurante_pratos copy.csv", ',', &meuCardapio_2);
 
-    // Chamar a função numero_linhas para obter o número de linhas
-    printf("Número de linhas lidas: %d\n", numero_linhas(&meuCardapio));
+      // Chamar a função numero_linhas para obter o número de linhas
+      printf("Número de linhas lidas: %d\n", numero_linhas(&meuCardapio));
 
-    //Imprimir na tela o cardapio
-    //exibeCSV(&meuCardapio);
-
-
- //_______________________________BUBBLE_______________________________________
-
-    clock_t t, t2;
-
-    //Ordenar por bolha
-   // bSort(&meuCardapio);
-    //bSort truncado - numero de linhas a ser impressa
-    printf("Os 4 pratos com maiores prioridades são: \n");
-    t=clock();
-    bSortT(&meuCardapio,5);
-    t=clock()-t;
-
-    double tempo = ((double)t) / CLOCKS_PER_SEC;
-
-    printf("o tempo de excecução do bubble sort foi em segundos: %.8f\n", tempo);
-    
+      //Imprimir na tela o cardapio
+      //exibeCSV(&meuCardapio);
 
 
-    printf("Os 4 pratos com maiores prioridades são: \n");
-    t2=clock();
-    bSortT(&meuCardapio,5);
-    t2=clock()-t2;
+   //_______________________________BUBBLE_______________________________________
 
-    double tempo2 = ((double)t2) / CLOCKS_PER_SEC;
+      clock_t t, t2;
 
-    printf("o tempo de excecução do quick sort foi em segundos: %.8f\n", tempo2);
+      //Ordenar por bolha
+      // bSort(&meuCardapio);
+      //bSort truncado - numero de linhas a ser impressa
+      printf("Os 4 pratos com maiores prioridades são: \n");
+      t=clock();
+      bSortT(&meuCardapio,5);
+      t=clock()-t;
 
-    qSortT(&meuCardapio_2, 5);
+      double tempo = ((double)t) / CLOCKS_PER_SEC;
 
-    return 0;
-
- //_______________________________QUICK_______________________________________
-
+      printf("o tempo de excecução do bubble sort foi em segundos: %.8f\n", tempo);
+      
 
 
+   //_______________________________QUICK_______________________________________
 
 
-}
+      printf("Os 4 pratos com maiores prioridades são: \n");
+      t2=clock();
+      qSortT(&meuCardapio_2,5);
+      t2=clock()-t2;
+
+      double tempo2 = ((double)t2) / CLOCKS_PER_SEC;
+      
+      printf("o tempo de excecução do quick sort foi em segundos: %.8f\n", tempo2);
+
+      return 0;
+
+
+   }
